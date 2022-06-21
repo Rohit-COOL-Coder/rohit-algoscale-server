fdd760350316df31a07fec977458a5f29713221c
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-21 21:15:32 +0530

    encryption added

--- route/employee.js
+++ route/employee.js
@@ -1,10 +1,14 @@
 const Employee = require("../model/Employee")
+const CryptoJS=require("crypto-js")
 
 const Route=require("express").Router()
 
 Route.post('/registration', async(req,res)=>{
-    const emp=new Employee(req.body)
-    // var ciphertext = CryptoJS.AES.encrypt( , 'secret key 123').toString();
+    const emp=new Employee({
+        employeeName:req.body.employeeName,
+        employeeEmail:req.body.employeeEmail,
+        employeePassword:CryptoJS.AES.encrypt(req.body.employeePassword,process.env.CRYPTOJS_KEY).toString()
+    })
         try{
        const data=await emp.save()
         res.status(200).json(data)
@@ -12,5 +16,29 @@ Route.post('/registration', async(req,res)=>{
             console.log("some error")
     }
 })
+
+Route.post("/login", async (req,res)=>{
+    try{
+      const user= await Employee.findOne({employeeEmail:req.body.email})
+      console.log(user)
+    !user && res.status(400).json("invalid Email")
+  
+    const hashedPassword=CryptoJS.AES.decrypt(user.employeePassword,process.env.CRYPTOJS_KEY)
+    const decryptPassword=hashedPassword.toString(CryptoJS.enc.Utf8)
+  
+    decryptPassword !==req.body.password && res.status(400).json("invalid Password")
+  
+    const accessToken=jwt.sign({
+      name:user.employeeName,
+      email:user.employeeEmail
+    },process.env.JWT_KEY,{expiresIn:"3d"})
+  
+    const {employeePassword,...other}=user._doc
+    
+    res.status(200).json({...other,accessToken})
+    }catch(err){
+      res.status(500).json(err)
+    }
+  })
 
 module.exports=Route
